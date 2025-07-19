fb0a88d59650e9a14d5405cc79e17015086fb1af
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-19 20:58:25 +0300

    .

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     name = models.CharField(max_length=100)
