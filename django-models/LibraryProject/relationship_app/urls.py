d0700143ac89a41798104b0069c5c48b5270521a
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-19 22:21:43 +0300

    added

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('library/details/<int:library_id>/', views.library_detail_view, name='library-detail-fbv'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail-cbv'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic.detail import DetailView
+from .models import Library
 # Create your views here.
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  # Access ManyToManyField
+        return context
