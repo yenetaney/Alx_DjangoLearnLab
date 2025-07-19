653b12b37b2c379bcc437b0646ce3c76d400f55e
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-19 20:52:31 +0300

    .

---
+++ LibraryProject/relationship_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='book')
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    book = models.ManyToManyField(Book, related_name='library')
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
