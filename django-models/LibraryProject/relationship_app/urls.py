66ec7e34dd28194ef9b80e0e53021cf6044eb714
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-19 22:25:44 +0300

    '

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import list_books
 
 urlpatterns = [
     path('library/details/<int:library_id>/', views.library_detail_view, name='library-detail-fbv'),
