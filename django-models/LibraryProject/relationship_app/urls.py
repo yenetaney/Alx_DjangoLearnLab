178583b31cfed21fbb59bd0649bf518a4742c61d
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-20 18:12:56 +0300

    .

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,6 +16,9 @@ urlpatterns = [
      path('books/add/', views.add_book, name='add_book'),
     path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
     path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+
 ]
 
 
