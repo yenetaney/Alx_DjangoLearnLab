002c124e93b39c9bbc809647886a42299e59ece2
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-20 17:59:24 +0300

    .

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -40,12 +40,13 @@ def check_role(role):
 
 @check_role('Admin')
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 @check_role('Librarian')
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @check_role('Member')
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
+
