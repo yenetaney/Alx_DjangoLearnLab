e075b28f55c32c129fb3077d9c838820312bb8fe
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-20 17:39:59 +0300

    added task 2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('library/details/<int:library_id>/', views.library_detail_view, name='library-detail-fbv'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail-cbv'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Library, Book 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 # Create your views here.
 
 class LibraryDetailView(DetailView):
@@ -16,3 +19,14 @@ class LibraryDetailView(DetailView):
     def list_books_view(request):
         books = Book.objects.all()
         return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user
+            return redirect('home')  # Change 'home' to your desired redirect
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
