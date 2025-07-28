d95e6cf2b6cf5f17946582a38cea067a9317fcb6
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-28 12:12:40 +0300

    updated

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import ListView
+from .models import Book
+
+# Function-based view example:
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+# OR Class-based view with permission and raise_exception:
+
+class BookListView(PermissionRequiredMixin, ListView):
+    model = Book
+    template_name = 'bookshelf/book_list.html'
+    context_object_name = 'books'
+    permission_required = 'bookshelf.view_book'
+    raise_exception = True
