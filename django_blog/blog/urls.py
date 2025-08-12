91adbb79b114658ffda1797895bca11b6d7e4540
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-12 19:23:46 +0300

    B

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+
 ]
