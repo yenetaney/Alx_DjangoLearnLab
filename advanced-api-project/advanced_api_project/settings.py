2ead5d972043f00cb7ccbda56b51dd374ff3e0f4
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-12 06:56:08 +0300

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_filters',
     'api',
     'rest_framework',
     'django.contrib.admin',
@@ -50,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
+
 
 ROOT_URLCONF = 'advanced_api_project.urls'
 
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,13 +3,33 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
+import django_filters
+from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 
 # Create your views here.
+class BookFilter(FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains')
+    author = django_filters.CharFilter(lookup_expr='icontains')
+    publication_year = django_filters.NumberFilter(field_name='published_date', lookup_expr='year')
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'published_date']
+    ordering = ['title']
+
 
 class BookDetailView(generics.ListAPIView):
     queryset = Book.objects.all()
