8397f18adf6fb1654f0afcfd81af291719edf5e7
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-12 06:44:13 +0300

    updated views.py from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 
