aca56561a569f5f222c0d9c3e80c29b15593613f
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-18 15:09:51 +0300

    updated

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -12,7 +12,7 @@ class UserRegistrationView(APIView):
         serializer = UserRegistrationSerializer(data = request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token = Token.objects.get(user=user)
+            token, _ = Token.objects.get_or_create(user=user)
             return Response(
                 {"token": token.key, "username":user.username}, status=status.HTTP_201_CREATED
             )
@@ -22,11 +22,11 @@ class UserLoginView(APIView):
     def post(self, request):
         serializer = UserLoginSerializer(data= request.data)
         if serializer.is_valid():
-            username = serializer.validate_data['username']
-            password = serializer.validate_data['password']
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
             user = authenticate(username = username, password = password)
             if user:
-                token, created = Token.objects.get_or_create(user=user)
+                token, _ = Token.objects.get_or_create(user=user)
                 return Response({"token":token.key, "username": user.username})
             return Response({"error": "invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
