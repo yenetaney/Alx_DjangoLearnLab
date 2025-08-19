e7056b5912127a52f06d8f5778e35f5520b59520
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-19 09:49:56 +0300

    Create Views for CRUD Operations

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework import viewsets, permissions 
+from .models import Post, Comment
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+
+class CreatePostView(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
