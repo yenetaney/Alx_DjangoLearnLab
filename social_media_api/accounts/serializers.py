c4631c0d1762c0982530cff16aadfeb92486968b
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-19 07:51:50 +0300

    updated

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -26,19 +26,21 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(required= True, write_only= True)
+    username = serializers.CharField(required=True)
     email = serializers.EmailField(required=True)
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None),
         )
+
 
         Token.objects.create(user=user)
         return user
