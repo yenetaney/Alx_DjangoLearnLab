0672aea5cb98e561d4d382d947cab588c285a543
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-19 22:28:47 +0300

    updated acounts urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -11,8 +11,9 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    path('', include(router.urls)),
-
+     path('follow/<int:user_id>', UserViewSet.as_view({'post': 'follow'}), name='follow-user'),
+    path('unfollow/<int:user_id>/', UserViewSet.as_view({'post': 'unfollow'}), name='unfollow-user'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
