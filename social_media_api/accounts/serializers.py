c8572b4a2d9f78d112ede25dbef4b6c0fa8ea204
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-19 07:49:13 +0300

    '

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -32,16 +32,17 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User( 
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
+            password=validated_data['password'],
             bio=validated_data.get('bio', ''),
-            profile_picture = validated_data.get('profile_picture', None),
+            profile_picture=validated_data.get('profile_picture', None),
         )
-        user.set_password(validated_data['password'])
-        user.save()
+
         Token.objects.create(user=user)
         return user
+
     
     def validate_username(self, value):
         if User.objects.filter(username=value).exists():
