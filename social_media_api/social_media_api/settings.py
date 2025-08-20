7307aa79447abb195092642dc528be06b9d8c0c0
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-20 20:17:58 +0300

    database

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -99,11 +99,14 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',
+        'PORT': 'your_db_port',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
